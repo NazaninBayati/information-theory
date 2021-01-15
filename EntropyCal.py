ab6abc1412c9dfe65106b50a7de1592e3e3f9b3b
NazaninBayati <54472723+NazaninBayati@users.noreply.github.com>
2021-01-15 12:54:00 +0330

    Add files via upload

--- EntropyCal.py
+++ EntropyCal.py
@@ -18,7 +18,8 @@ for item in df:
     data = df[item]
     pd_series = pd.Series(data)
     counts = pd_series.value_counts()
-    entropy = stats.entropy(counts)
+    total = sum(counts)
+    entropy = stats.entropy(counts/total, base=2)
     print(entropy)
     if str(item) not in dictionary:
         dictionary[str(item)] = []
