3659fc0720448b253668c3f36fa5e7c36acbc4fa
NazaninBayati <54472723+NazaninBayati@users.noreply.github.com>
2021-01-16 12:19:36 +0330

    Add files via upload

--- EntropyCal.py
+++ EntropyCal.py
@@ -1,7 +1,7 @@
 from scipy import stats
 from scipy.io import arff
 import pandas as pd
-
+from tqdm import tqdm
 ###################################
 # Entropy Of Each Feature
 ###################################
@@ -14,13 +14,13 @@ dictionary = {}
 SF = 40
 i = 1
 entropy_list = []
-for item in df:
+for item in tqdm(df):
     data = df[item]
     pd_series = pd.Series(data)
     counts = pd_series.value_counts()
     total = sum(counts)
     entropy = stats.entropy(counts/total, base=2)
-    print(entropy)
+   # print(entropy)
     if str(item) not in dictionary:
         dictionary[str(item)] = []
     dictionary[str(item)] = entropy
