619849a03194a370d882c713394271d4656cad0a
NazaninBayati <54472723+NazaninBayati@users.noreply.github.com>
2021-01-17 22:47:19 +0330

    Kendall

---
+++ kendallCC.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from tqdm import tqdm
+from scipy import stats
+
+###################################
+# Pearson correlation coefficient and p-value Of Features
+###################################
+df = pd.read_csv('Training Dataset.csv')
+#data = arff.loadarff('Training Dataset original.arff')
+#df = pd.DataFrame(data[0])
+df.head()
+
+dictionary = {}
+SF = 40
+i = 1
+entropy_list = []
+#print(df[0])
+listt = df.keys()
+
+for item in tqdm(df):
+    j = 1
+    for i in range( listt[j:listt.__len__()-1].__len__()):
+
+        X = df[item].values
+        Y = df[listt[i]].values
+        a = stats.kendalltau(X,Y)
+        print(a)
+
+        if str([item,listt[i],listt[i+1]]) not in dictionary:
+            dictionary[str([item,listt[i],listt[i+1]])]=[]
+        dictionary[str([item,listt[i],listt[i+1]])] = a[0]
+        #print(dictionary[str([item,i])] )
+        j = j+1
+
